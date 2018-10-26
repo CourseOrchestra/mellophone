c62bc54500fe8363a94d67e9c0e7436918300be2
s.borodanev <boss@curs.ru>
2018-10-26 11:44:00 +0300

    Upgrade tests to JUnit5

--- src/test/java/ru/curs/mellophone/test/XMLLoginProviderTest.java
+++ src/test/java/ru/curs/mellophone/test/XMLLoginProviderTest.java
@@ -1,9 +1,7 @@
 package ru.curs.mellophone.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 import ru.curs.mellophone.logic.AuthManager;
 import ru.curs.mellophone.logic.XMLLoginProvider;
